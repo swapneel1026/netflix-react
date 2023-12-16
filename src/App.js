269f9a31b55d05d91520fb58e8a11865d3d504fd
swapneel1026 <swapneelshubham44@gmail.com>
2023-12-16 19:29:07 +0530

    cleanup:remove demo store code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import { useDispatch, useSelector } from "react-redux";
-import { decreaseCount, increasecount } from "./Store/Slices/CountSlice";
 function App() {
-  const dispatch = useDispatch();
-  const count = useSelector((store) => store.count.count);
-  return (
-    <div className="text-red-500">
-      <h1>Count:{count}</h1>
-      <button
-        onClick={() => {
-          dispatch(increasecount());
-        }}
-        className="outline py-2 px-6 m-2 bg-green-400"
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          dispatch(decreaseCount());
-        }}
-        className="outline py-2 px-6 m-2 bg-green-400"
-      >
-        -
-      </button>
-    </div>
-  );
+  return <div className="text-red-500"></div>;
 }
 
 export default App;
