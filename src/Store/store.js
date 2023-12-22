16e0a3a10fe76d7a84ef5845ee911baa29d83c4c
swapneel1026 <swapneelshubham44@gmail.com>
2023-12-22 20:23:33 +0530

    fixed:login/logout logic

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CountSlice from "./Slices/CountSlice";
+import LoggedInSlice from "./Slices/LoggedInSlice";
 const store = configureStore({
   reducer: {
     count: CountSlice,
+    loggedin:LoggedInSlice
   },
 });
 export default store;
