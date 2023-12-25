164f4b87c5855e8d7553b17c09906679016fdac3
swapneel1026 <swapneelshubham44@gmail.com>
2023-12-25 11:25:44 +0530

    remove unused vars

--- src/components/LandingPage/NavigationBar.jsx
+++ src/components/LandingPage/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import NetflixIcon from "../../assets/IconsSvg/NetflixLogo";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
